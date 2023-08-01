c3ce283daf0bce983aece8eb55a3222cee311a70
NowWeAre-Free <kontodarm@wp.pl>
2023-08-01 21:19:30 +0200

    add second program option - only with seconds and miliseconds

--- main.js
+++ main.js
@@ -3,33 +3,61 @@
 const mainButton = document.querySelector(".main");
 const resetButton = document.querySelector(".reset");
 const display = document.querySelector(".time div");
-let hours = 0;
-let minutes = 0;
-let seconds = 0;
-let interval;
+
+// let hours = 0;
+// let minutes = 0;
+// let seconds = 0;
+
+let flag = false;
+let intervalID;
+let time = 0;
+
+// const startStopwatch = () => {
+//   if (mainButton.textContent === "Start") {
+//     interval = setInterval(() => {
+//       seconds++;
+//       if (seconds > 59) {
+//         minutes++;
+//         seconds = 0;
+//       }
+//       if (minutes > 59) {
+//         hours++;
+//         minutes = 0;
+//       }
+//       display.textContent = `${hours < 10 ? `0${hours}` : hours}:${
+//         minutes < 10 ? `0${minutes}` : minutes
+//       }:${seconds < 10 ? `0${seconds}` : seconds}`;
+//     }, 10);
+
+//     mainButton.textContent = "Pauza";
+//   } else if (mainButton.textContent === "Pauza") {
+//     clearInterval(interval);
+//     mainButton.textContent = "Start";
+//   }
+// };
+
+// mainButton.addEventListener("click", startStopwatch);
+// resetButton.addEventListener("click", () => {
+//   clearInterval(interval);
+//   display.textContent = "---";
+
+// });
 
 const startStopwatch = () => {
-  if (mainButton.textContent === "Start") {
-    interval = setInterval(() => {
-      seconds++;
-      if (seconds > 59) {
-        minutes++;
-        seconds = 0;
-      }
-      if (minutes > 59) {
-        hours++;
-        minutes = 0;
-      }
-      display.textContent = `${hours < 10 ? `0${hours}` : hours}:${
-        minutes < 10 ? `0${minutes}` : minutes
-      }:${seconds < 10 ? `0${seconds}` : seconds}`;
-    }, 1000);
-
+  if (!flag) {
+    flag = !flag;
     mainButton.textContent = "Pauza";
-  } else if (mainButton.textContent === "Pauza") {
-    clearInterval(interval);
+    intervalID = setInterval(start, 10);
+  } else {
+    flag = !flag;
     mainButton.textContent = "Start";
+    clearInterval(intervalID);
   }
+};
+
+const start = () => {
+  time++;
+  display.textContent = (time / 100).toFixed(2);
 };
 
 mainButton.addEventListener("click", startStopwatch);
