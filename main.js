18ca196d1df3113d1d45dbbd6f1d5316a108e577
NowWeAre-Free <kontodarm@wp.pl>
2023-08-01 20:30:41 +0200

    add buttons start/stop and reset and add logic to it

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
 // http://websamuraj.pl/examples/js/projekt11/
+
+const mainButton = document.querySelector(".main");
+const resetButton = document.querySelector(".reset");
+const display = document.querySelector(".time div");
+let number = 0;
+let interval;
+
+const startStopwatch = () => {
+  if (mainButton.textContent === "Start") {
+    interval = setInterval(() => {
+      number++;
+      display.textContent = number < 10 ? `0${number}` : number;
+    }, 1000);
+    mainButton.textContent = "Pauza";
+  } else if (mainButton.textContent === "Pauza") {
+    clearInterval(interval);
+    mainButton.textContent = "Start";
+  }
+};
+
+mainButton.addEventListener("click", startStopwatch);
+resetButton.addEventListener("click", () => {
+  clearInterval(interval);
+  display.textContent = "---";
+});
